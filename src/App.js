aca798af3310c91aba635a37297aabfe6014ae11
UriProtocol <angel_3041210085@utd.edu.mx>
2023-01-19 11:33:04 -0600

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import './App.css'
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => props.utd ? "#4ea586" : props.gh ? "#171b21": props.fb ? "#4fa8f6" : "white"};
+  color: ${props => props.utd ? "white" : props.gh ? "#f1f6fb" : props.fb ? "#fff" : "black"};
+
+  font-size: 1.5em;
+  font-weight: 500;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid ${props => props.utd ? "#357565" : props.gh ? "#020409" : props.fb ? "#2a63d9" : "black"};
+  border-radius: 3px;
+  cursor: pointer;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <h1>Botones</h1>
+      <div className='buttons-div'>
+        <a href='https://www.utd.edu.mx'><Button utd>UTD</Button></a>
+        <a href='https://www.github.com/UriProtocol'><Button gh>GitHub</Button></a>
+        <a href='https://www.chess.com/member/uriknight03'><Button fb>Red social</Button></a>
+      </div>
     </div>
   );
 }
